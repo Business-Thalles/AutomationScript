587c00c2ea856d350223f9313480fbe009331042
F193865 <F193865@STDENGCTL-FONTA>
2023-05-30 15:55:23 -0300

    Adicionar arquivos de projeto.

---
+++ Thalles2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Thalles2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Thalles2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("79AED553-E156-4A52-A9D4-8F9E9F6011B3")]
+[assembly: AssemblyVersion("1.0.0.0")]
